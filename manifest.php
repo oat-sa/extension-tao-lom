3de2f71fda829673dc5ba3a48466baf30eb7ee39
cmoyon <camille@taotesting.com>
2016-11-17 16:54:13 +0100

    Init tao folders

---
+++ manifest.php
@@ -0,0 +1,67 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
+
+/**
+ * Generated using taoDevTools 2.15.0
+ */
+return array(
+    'name' => 'taoLom',
+    'label' => 'LOM',
+    'description' => 'Learning Object Meta-data',
+    'license' => 'GPL-2.0',
+    'version' => '0.1.0',
+    'author' => 'Open Assessment Technologies SA',
+    'requires' => array(
+        'tao' => '>=7.26.0'
+    ),
+    'managementRole' => 'http://www.tao.lu/Ontologies/generis.rdf#taoLomManager',
+    'acl' => array(
+        array('grant', 'http://www.tao.lu/Ontologies/generis.rdf#taoLomManager', array('ext'=>'taoLom')),
+    ),
+    'install' => array(
+        'rdf' => array(
+            dirname(__FILE__) . '/install/ontology/lom.rdf',
+            dirname(__FILE__) . '/install/ontology/category.rdf',
+            dirname(__FILE__) . '/install/ontology/general.rdf',
+            dirname(__FILE__) . '/install/ontology/lifecycle.rdf',
+            dirname(__FILE__) . '/install/ontology/metadata.rdf',
+
+        )
+    ),
+    'uninstall' => array(
+    ),
+    'routes' => array(
+        '/taoLom' => 'oat\\taoLom\\controller'
+    ),
+    'update' => \oat\taoLom\scripts\update\Updater::class,
+    'constants' => array(
+        # views directory
+        "DIR_VIEWS" => dirname(__FILE__).DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR,
+        
+        #BASE URL (usually the domain root)
+        'BASE_URL' => ROOT_URL.'taoLom/',
+        
+        #BASE WWW required by JS
+        'BASE_WWW' => ROOT_URL.'taoLom/views/'
+    ),
+    'extra' => array(
+        'structures' => dirname(__FILE__).DIRECTORY_SEPARATOR.'controller'.DIRECTORY_SEPARATOR.'structures.xml',
+    )
+);
