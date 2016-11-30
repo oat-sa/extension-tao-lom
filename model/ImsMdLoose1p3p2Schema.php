9f3888c2b3ca49ce83778cd571da090fbc4c90a0
= <jerome@taotesting.com>
2016-11-30 09:47:03 +0100

    Added constant interfaces for Ontology and Schemata.

---
+++ model/ImsMdLoose1p3p2Schema.php
@@ -0,0 +1,30 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA;
+ *
+ */
+namespace oat\taoLom\model;
+
+interface ImsMdLoose1p3p2Schema
+{
+    const PATH_ROOT = 'http://ltsc.ieee.org/xsd/LOM#lom';
+    const PATH_GENERAL = 'http://ltsc.ieee.org/xsd/LOM#general';
+    const PATH_IDENTIFIER = 'http://ltsc.ieee.org/xsd/LOM#identifier';
+    const PATH_ENTRY = 'http://ltsc.ieee.org/xsd/LOM#entry';
+    const PATH_TITLE = 'http://ltsc.ieee.org/xsd/LOM#title';
+    const PATH_STRING = 'http://ltsc.ieee.org/xsd/LOM#string';
+}
