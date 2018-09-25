f8e87f83cd51b452c677ccd631177a327625b74e
Péter Halász <halaszpeterzsolt@gmail.com>
2018-09-25 09:37:15 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label'       => 'Learning Object Metadata',
     'description' => 'An extension supporting the IMS Global Loose version of the Learning Object Metadata standard.',
     'license'     => 'GPL-2.0',
-    'version'     => '1.0.1',
+    'version'     => '1.0.2',
     'author'      => 'Open Assessment Technologies SA',
     'requires' => array(
         'tao' => '>=9.0.0',
